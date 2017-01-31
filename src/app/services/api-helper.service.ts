ab61b4e381bf6ec06bdae8c02bdf3fc6af44ce91
hoangl8696 <lyhoang080696@gmail.com>
2017-01-31 15:03:02 +0200

    refine login, add signup, have not test signup

--- src/app/services/api-helper.service.ts
+++ src/app/services/api-helper.service.ts
@@ -16,7 +16,10 @@ export class ApiHelperService {
     this.options = new RequestOptions({ headers: this.headers });
      if (contentType) {
       this.headers.append('Content-type', contentType);
+    } else {
+      this.headers.append('Content-type', 'application/json')
     }
+
     if (token) {
       this.headers.append('x-access-token', token);
     }
@@ -25,9 +28,17 @@ export class ApiHelperService {
 
   login: any = (data) => {
     const url = this.baseUrl + 'login/';
-    const options = this.headerBuilder('application/json');
+    const options = this.headerBuilder();
     const body = JSON.stringify(data);
     console.log (body);
     return this.http.post(url,body,options);
+  }
+
+  signup: any = (data) => {
+    const url = this.baseUrl + 'users/';
+    const options = this.headerBuilder;
+    const body = JSON.stringify(data);
+    console.log (body);
+    return this.http.post(url, body, options);
   }
 }
