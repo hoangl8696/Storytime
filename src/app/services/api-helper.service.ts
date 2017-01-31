a50d01aa036d8a20d8a64c92a374072a85afdf0e
hoangl8696 <lyhoang080696@gmail.com>
2017-01-31 12:23:38 +0200

    create api helper services, create imports modify app module to hook the service up

---
+++ src/app/services/api-helper.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import {Headers, Http, RequestOptions, Response} from '@angular/http'
+
+@Injectable()
+export class ApiHelperService {
+
+  constructor() { }
+
+}
